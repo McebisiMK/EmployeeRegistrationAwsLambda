76d057020dce4920bb3cf775e247001f5b6feed6
Mcebisi Mkhohliwe <mcebisimkhohliwe@gmail.com>
2022-04-13 17:46:25 +0200

    Added Entities/DTOs

---
+++ EmployeeRegistrationLambda/DTOs/UserDTO.cs
@@ -0,0 +1,9 @@
+namespace DevLearningAwsLambda.DTOs
+{
+    public class UserDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
